23eeb7b2bfad1ea2c974988af59427a7c465e1b8
JSebCort <44817309+JSebCort@users.noreply.github.com>
2021-01-25 19:52:26 -0500

    Update January 25.py

--- January LeetCoding Challenge 2021/January 25.py
+++ January LeetCoding Challenge 2021/January 25.py
@@ -1,7 +1,7 @@
-# Problem Name: Check If All 1's Are at Least Length K Places Away
-# Description: Given an array nums of 0s and 1s and an integer k, return True if all 1's are at least k places away from each other, otherwise return False.
-
 '''
+Problem Name: Check If All 1's Are at Least Length K Places Away
+Description: Given an array nums of 0s and 1s and an integer k, return True if all 1's are at least k places away from each other, otherwise return False.
+
 EXAMPLE
 Input: nums = [1,0,0,0,1,0,0,1], k = 2
 Output: true
