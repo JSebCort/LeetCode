d9f37b2a48067b4d9453f82861af5ad1140514ae
JSebCort <44817309+JSebCort@users.noreply.github.com>
2021-01-25 19:49:23 -0500

    Update January 25.py
    Added problem name, description and example.

--- January LeetCoding Challenge 2021/January 25.py
+++ January LeetCoding Challenge 2021/January 25.py
@@ -1,3 +1,13 @@
+# Problem Name: Check If All 1's Are at Least Length K Places Away
+# Description: Given an array nums of 0s and 1s and an integer k, return True if all 1's are at least k places away from each other, otherwise return False.
+
+'''
+EXAMPLE
+Input: nums = [1,0,0,0,1,0,0,1], k = 2
+Output: true
+Explanation: Each of the 1s are at least 2 places away from each other.
+'''
+
 class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
         
