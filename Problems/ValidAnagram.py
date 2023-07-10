efbe5aad97eec68bee2f2fb3954e2b29d84db532
JSebCort <sebastianjscg@gmail.com>
2023-07-10 11:59:35 -0400

    Added a second solution

--- Problems/ValidAnagram.py
+++ Problems/ValidAnagram.py
@@ -10,6 +10,7 @@ Input: s = "rat", t = "car"
 Output: false
 '''
 class Solution:
+    #Solition 1
     def isAnagram(self, s: str, t: str) -> bool:
         # If t is of greater length than s, then it cannot be an anagram of s.
         if len(t) != len(s):
@@ -32,3 +33,20 @@ class Solution:
         # Return whether all values in the dictionary are 0. This would mean that
         # all letter occurances were used in t.
         return all(value==0 for value in word.values())
+    
+    #Solution 2
+    def isAnagram2(self, s: str, t: str) -> bool:
+        # If t is of greater length than s, then it cannot be an anagram of s.
+        if len(s) != len(t):
+            return False
+        
+        # Create two dictionaries for each string.
+        dictS, dictT = {}, {}
+
+        # Iterate through both strings, adding +1 for every character that appears.
+        for i in range(len(s)):
+            dictS[s[i]] = 1 + dictS.get(s[i],0)
+            dictT[t[i]] = 1 + dictT.get(t[i],0)
+
+        # If the dictionaries match in keys & values, return True.
+        return dictS == dictT
